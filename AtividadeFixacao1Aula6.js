dffdb59a31b4961fb40e4b7068df738f1577171a
SamiraHolanda <137845751+SamiraHolanda@users.noreply.github.com>
2023-07-03 15:33:44 -0300

    03/07/2023

---
+++ AtividadeFixacao1Aula6.js
@@ -0,0 +1,24 @@
+//Questão1:
+var cores = ['Azul', 'Rosa', 'Amarelo', 'Verde', 'Laranja', 'Preto', 'Roxo', 'Branco', 'Vermelho', 'Marron'];
+console.log(cores.length);
+var posicao1 = cores[1];
+var posicao2 = cores[3];
+var posicao3 = cores[5];
+var posicao4 = cores[8];
+console.log(posicao1, posicao2, posicao3, posicao4);
+//Questão2:
+var nomeFilmes = ['A pequena sereia', 'A bela e a fera', 'Branca de neve e os sete anões', 'Cinderela', 'Mulan 2'];
+console.log(nomeFilmes.length);
+var posicao2 = nomeFilmes[2];
+var posicao4 = nomeFilmes[4];
+console.log(posicao2, posicao4);
+var posicao1 = nomeFilmes[1];
+console.log(posicao1);
+//Questão3:
+var musicas = ['Coração cigano', 'Te felicito', 'Positions', 'Por supuesto', 'Until I Found You', 'Die For You'];
+console.log(musicas.length);
+var posicao1 = musicas[1];
+var posicao2 = musicas[2];
+var posicao6 = musicas[6];
+var posicao8 = musicas[8];
+console.log(posicao1, posicao2, posicao6, posicao8);
